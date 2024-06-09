7d618852f1bea31835490eb595a41a0180cc1739
RitchielRez <ritchielrez@gmail.com>
2024-06-09 19:28:03 +0600

    feat!(rit_dyn_arr): a big rewrite
    This rewrite basically changes how internally a `rda` or dynamic array
    works. Instead of a metadata struct, now the metadata and the array is
    stored in a single struct generated by a macro.

--- rit_dyn_arr.h
+++ rit_dyn_arr.h
@@ -1,12 +1,18 @@
 // LICENSE
 // See end of the file for license information.
-
 #ifndef RIT_DYN_ARR_H_INCLUDED
 #define RIT_DYN_ARR_H_INCLUDED
+
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define DEFAULT_ARR_CAP 16
+
+// Disable MSVC warning 4702: unreachable code
+#pragma warning(disable : 4702)
 
 /// @brief Custom allocator interface.
 /// Functions allocating memory takes a custom allocator based off this
@@ -19,184 +25,207 @@ typedef struct {
                 // NULL if none
 } rda_allocator;
 
-/// @brief Holds the metadata about the array.
-/// This metadata struct is stored as header of the array
-typedef struct {
-  size_t m_size;
-  size_t m_capacity;
-  size_t m_objsize;
-} rda_metadata;
-
-/// @brief Get the pointer to the metadata header struct of an array.
-/// @param t_rda The array
-#define rda_get_metadata(t_rda) (&((rda_metadata *)t_rda)[-1])
-
-/// @internal
-inline void *rda_alloc(const char *t_file, int t_line, size_t t_objsize,
-                       size_t t_size, rda_allocator *t_allocator) {
-  rda_metadata *arr = (rda_metadata *)t_allocator->alloc(
-      t_allocator->m_ctx, sizeof(rda_metadata) + t_objsize * t_size * 2);
-  if (!arr) {
-    fprintf(stderr, "Error: allocation failed, file: %s, line: %d\n", t_file,
-            t_line);
-    exit(EXIT_FAILURE);
+/// @brief Dynamic array struct
+#define rda_struct(t_type) \
+  struct {                 \
+    size_t m_size;         \
+    size_t m_capacity;     \
+    size_t m_objsize;      \
+    t_type *m_data;        \
   }
-  arr->m_size = t_size;
-  arr->m_objsize = t_objsize;
-  arr->m_capacity = t_size * 2;
-  arr += 1;
-  return (void *)arr;
-}
 
-inline void rda_free(void *t_rda, rda_allocator *t_allocator) {
-  t_allocator->free(t_allocator->m_ctx, rda_get_metadata(t_rda));
-}
+#define rda_size(t_rda) t_rda.m_size
 
-inline size_t rda_size(void *t_rda) { return rda_get_metadata(t_rda)->m_size; }
+#define rda_capacity(t_rda) t_rda.m_capacity
 
-inline size_t rda_capacity(void *t_rda) {
-  return rda_get_metadata(t_rda)->m_capacity;
-}
-
-/// @brief Set the capacity of an array.
-#define rda_reserve(t_rda, t_new_capacity, t_allocator) \
-  rda_realloc(__FILE__, __LINE__, &t_rda, t_new_capacity, t_allocator)
-
-/// @internal
-inline void rda_realloc(const char *t_file, int t_line, void **t_rda,
-                        size_t t_new_capacity, rda_allocator *t_allocator) {
-  if (t_new_capacity > rda_capacity(*t_rda)) {
-    size_t objsize = rda_get_metadata(*t_rda)->m_objsize;
-    rda_metadata *arr = (rda_metadata *)t_allocator->realloc(
-        t_allocator->m_ctx, rda_get_metadata(*t_rda),
-        sizeof(rda_metadata) + objsize * rda_capacity(*t_rda),
-        sizeof(rda_metadata) + objsize * t_new_capacity);
-    if (!arr) {
-      fprintf(stderr, "Error: reallocation failed, file: %s, line: %d\n",
-              t_file, t_line);
-      exit(EXIT_FAILURE);
-    }
-    arr += 1;
-    *t_rda = (void *)arr;
-    rda_get_metadata(*t_rda)->m_capacity = t_new_capacity;
-  }
-}
-
-/// @internal
-inline bool rda_index_bounds_check(const char *t_file, int t_line, void *t_rda,
-                                   size_t t_index) {
-  if (t_index < rda_size(t_rda)) return true;
-  fprintf(stderr, "Error: array index is out of bounds, file: %s, line: %d\n",
-          t_file, t_line);
-  exit(EXIT_FAILURE);
-}
-
-/// @param Check if an array is empty.
-inline bool rda_empty(void *t_rda) { return rda_size(t_rda) == 0; }
-
-/// @brief Empty out an array.
-inline void rda_clear(void *t_rda) { rda_get_metadata(t_rda)->m_size = 0; }
-
-#define rda_swap(t_rda, t_rda_other) \
-  do {                               \
-    void *tmp = t_rda;               \
-    t_rda = t_rda_other;             \
-    t_rda_other = tmp;               \
+#define rda_init(t_rda, t_size, t_objsize, t_allocator)                   \
+  do {                                                                    \
+    size_t capacity =                                                     \
+        DEFAULT_ARR_CAP < t_size * 2 ? t_size * 2 : DEFAULT_ARR_CAP;      \
+    t_rda.m_data =                                                        \
+        (t_allocator)->alloc((t_allocator)->m_ctx, capacity * t_objsize); \
+    if (!t_rda.m_data) {                                                  \
+      fprintf(stderr, "Error: allocation failed, file: %s, line: %d\n",   \
+              __FILE__, __LINE__);                                        \
+      exit(EXIT_FAILURE);                                                 \
+    }                                                                     \
+    t_rda.m_size = t_size;                                                \
+    t_rda.m_capacity = capacity;                                          \
+    t_rda.m_objsize = t_objsize;                                          \
   } while (0)
 
-/// @brief Makes a non-binding request to make the capacity of an array equal to
+/// @brief Set the capacity of a array.
+#define rda_reserve(t_rda, t_new_capacity, t_allocator)                   \
+  if (t_new_capacity > rda_capacity(t_rda)) {                             \
+    t_rda.m_data = (t_allocator)                                          \
+                       ->realloc((t_allocator)->m_ctx, t_rda.m_data,      \
+                                 rda_capacity(t_rda) * t_rda.m_objsize,   \
+                                 t_new_capacity * t_rda.m_objsize);       \
+    if (!t_rda.m_data) {                                                  \
+      fprintf(stderr, "Error: reallocation failed, file: %s, line: %d\n", \
+              __FILE__, __LINE__);                                        \
+      exit(EXIT_FAILURE);                                                 \
+    }                                                                     \
+    t_rda.m_capacity = t_new_capacity;                                    \
+  }
+
+#define rda_swap(t_rda, t_rda_other)              \
+  do {                                            \
+    size_t tmp_size = rda_size(t_rda);            \
+    t_rda.m_size = rda_size(t_rda_other);         \
+    t_rda_other.m_size = tmp_size;                \
+    size_t tmp_capacity = rda_capacity(t_rda);    \
+    t_rda.m_capacity = rda_capacity(t_rda_other); \
+    t_rda_other.m_capacity = tmp_capacity;        \
+    void *tmp_data = rda_data(t_rda);             \
+    t_rda.m_data = rda_data(t_rda_other);         \
+    t_rda_other.m_data = tmp_data;                \
+  } while (0)
+
+/// @brief Makes a non-binding request to make the capacity of a array equal to
 /// its size. In this library this is definied as a no-op function.
-inline void shrink_to_fit(void *t_rda) { (void)t_rda; }
+#define rda_shrink_to_fit(t_rda) (void)t_rda
 
-/// @brief Initialize a rda.
-#define rda(t_type, t_rda, t_size, t_allocator)                           \
-  t_type *t_rda = (t_type *)rda_alloc(__FILE__, __LINE__, sizeof(t_type), \
-                                      t_size, t_allocator)
+/// @brief Returns a pointer to the internal data of the rda struct.
+#define rda_data(t_rstr) t_rstr.m_data
 
-#define rda_at(t_rda, t_index)                                 \
-  (rda_index_bounds_check(__FILE__, __LINE__, t_rda, t_index)) \
-      ? t_rda[t_index]                                         \
-      : t_rda[t_index]
+#define rda_free(t_rda, t_allocator) \
+  (t_allocator)->free((t_allocator)->m_ctx, t_rda.m_data)
 
-#define rda_set(t_rda, t_index)                                     \
-  if (rda_index_bounds_check(__FILE__, __LINE__, t_rda, t_index)) { \
-  }                                                                 \
-  t_rda[t_index]
+/// @param Check if a array is empty.
+#define rda_empty(t_rda) rda_size(t_rda) == 0
+
+/// @brief Empty out a array.
+#define rda_clear(t_rda) t_rda.m_size = 0
+
+/// @brief Create a rda.
+#define rda(t_type, t_rda, t_size, t_allocator) \
+  rda_struct(t_type) t_rda = {};                \
+  rda_init(t_rda, t_size, sizeof(t_type), t_allocator)
 
 /// @param t_rda Where to copy
 /// @param t_rda_other What to copy
-#define rda_copy(t_rda, t_rda_other, t_allocator)             \
-  rda_reserve(t_rda_other, rda_capacity(t_rda), t_allocator); \
-  rda_get_metadata(t_rda_other)->m_size = rda_size(t_rda);    \
-  for (size_t i = 0; i < rda_size(t_rda); i++) {              \
-    rda_set(t_rda_other, i) = rda_at(t_rda, i);               \
+/// @param t_size The size of subarray of t_rda_other
+#define rda_cp(t_type, t_rda, t_rda_other, t_index, t_size, t_allocator)       \
+  rda_struct(t_type) t_rda = {};                                               \
+  if (t_index > rda_size(t_rda_other)) {                                       \
+    fprintf(stderr,                                                            \
+            "Error: starting index of subarray out of bounds of the array, "   \
+            "file: %s, line: %d\n",                                            \
+            __FILE__, __LINE__);                                               \
+    exit(EXIT_FAILURE);                                                        \
+  } else if (t_index + t_size > rda_size(t_rda_other)) {                       \
+    fprintf(stderr,                                                            \
+            "Error: size of subarray greater than the array, file: %s, line: " \
+            "%d\n",                                                            \
+            __FILE__, __LINE__);                                               \
+    exit(EXIT_FAILURE);                                                        \
+  }                                                                            \
+  rda_init(t_rda, t_size - t_index, sizeof(t_type), t_allocator);              \
+  for (size_t i = 0, j = t_index; i < t_size; i++, j++) {                      \
+    rda_at(t_rda, i) = rda_at(t_rda_other, j);                                 \
   }
 
+#define rda_ret_ptr_at_index(t_rda, t_index)                                \
+  ((t_index >= rda_size(t_rda) && t_index < 0)                              \
+       ? (fprintf(stderr,                                                   \
+                  "Error: array index out of bounds, file: %s, line: %d\n", \
+                  __FILE__, __LINE__),                                      \
+          exit(EXIT_FAILURE), &(t_rda.m_data[t_index]))                     \
+       : &(t_rda.m_data[t_index]))
+
+#define rda_at(t_rda, t_index) (*(rda_ret_ptr_at_index(t_rda, t_index)))
+
 /// @brief Get the pointer to the first element of an array
-#define rda_begin(t_rda) (&(t_rda[0]))
+#define rda_begin(t_rda) (&(t_rda.m_data[0]))
 /// @brief Get the pointer to the past-the-end element of an array
-#define rda_end(t_rda) (&(t_rda[rda_size(t_rda)]))
+#define rda_end(t_rda) (&(t_rda.m_data[rda_size(t_rda)]))
 
 /// @brief Get the first element of an array
-#define rda_front(t_rda) (t_rda[0])
+#define rda_front(t_rda) (t_rda.m_data[0])
 /// @brief Get the last element of an array
-#define rda_back(t_rda) (t_rda[rda_size(t_rda) - 1])
+#define rda_back(t_rda) (t_rda.m_data[rda_size(t_rda) - 1])
 
-#define rda_push_back(t_rda, t_value, t_allocator)              \
+#define rda_push_back(t_rda, t_val, t_allocator)                \
   if (rda_capacity(t_rda) <= rda_size(t_rda) + 1) {             \
     rda_reserve(t_rda, (rda_size(t_rda) + 1) * 2, t_allocator); \
   }                                                             \
-  rda_get_metadata(t_rda)->m_size++;                            \
-  rda_set(t_rda, rda_size(t_rda) - 1) = t_value
+  t_rda.m_data[rda_size(t_rda)] = t_val;                        \
+  t_rda.m_size++
 
-#define rda_pop_back(t_rda) rda_get_metadata(t_rda)->m_size--
+#define rda_pop_back(t_rda) t_rda.m_size--
 
-#define rda_append(t_rda, t_count, t_value, t_allocator) \
-  for (int i = 1; i <= t_count; i++) {                   \
-    rda_push_back(t_rda, t_value, t_allocator);          \
+#define rda_append_val(t_rda, t_size, t_val, t_allocator) \
+  for (size_t i = 1; i <= t_size; i++) {                  \
+    rda_push_back(t_rda, t_val, t_allocator);             \
   }
 
-#define rda_resize(t_rda, t_count, t_value, t_allocator) \
-  rda_clear(t_rda);                                      \
-  rda_append(t_rda, t_count, t_value, t_allocator)
+#define rda_append_arr(t_rda, t_arr, t_allocator)               \
+  for (size_t i = 0; i < sizeof(t_arr) / sizeof(arr[0]); i++) { \
+    rda_push_back(t_rda, t_arr[i], t_allocator);                \
+  }
 
-/// @brief Insert a value in the array before t_pos.
-#define rda_insert(t_rda, t_pos, t_value, t_allocator)    \
-  rda_push_back(t_rda, 0, t_allocator);                   \
-  for (size_t i = rda_size(t_rda) - 1; i >= t_pos; i--) { \
-    rda_set(t_rda, i) = rda_at(t_rda, i - 1);             \
-    rda_set(t_rda, i - 1) = 0;                            \
-  }                                                       \
-  rda_set(t_rda, t_pos - 1) = t_value
+/// @brief Remove elements from the end of the array
+#define rda_remove(t_rda, t_size)        \
+  for (size_t i = 1; i <= t_size; i++) { \
+    rda_pop_back(t_rda);                 \
+  }
 
-/// @brief Remove the value in the array at t_pos.
-#define rda_erase(t_rda, t_pos)                          \
-  for (size_t i = t_pos + 1; i < rda_size(t_rda); i++) { \
-    rda_set(t_rda, i - 1) = rda_at(t_rda, i);            \
-  }                                                      \
-  rda_pop_back(t_rda)
+/// @brief Changes the number of characters stored
+#define rda_resize(t_rda, t_size, t_val, t_allocator) \
+  rda_clear(t_rda);                                   \
+  rda_append_val(t_rda, t_size, t_val, t_allocator)
+
+/// @brief Insert characters in the array at t_index.
+#define rda_insert(t_rda, t_index, t_size, t_val, t_allocator)         \
+  do {                                                                 \
+    rda_append_val(t_rda, t_size, t_val, t_allocator);                 \
+    for (size_t i = rda_size(t_rda) - 1; i >= t_index + t_size; i--) { \
+      rda_at(t_rda, i) = rda_at(t_rda, i - t_size);                    \
+      rda_at(t_rda, i - t_size) = t_val;                               \
+    }                                                                  \
+  } while (0)
+
+/// @brief Remove characters in the array at t_index.
+#define rda_erase(t_rda, t_index, t_size)                         \
+  do {                                                            \
+    for (size_t i = t_index + t_size; i < rda_size(t_rda); i++) { \
+      rda_at(t_rda, i - t_size) = rda_at(t_rda, i);               \
+    }                                                             \
+    rda_remove(t_rda, t_size);                                    \
+  } while (0)
+
+/// @param t_rda Where to assign
+/// @param t_rsv What to assign
+// #define rda_assign(t_rda, t_rsv, t_allocator) \
+//   rda_clear(t_rda);                           \
+//   rda_append_str(t_rda, t_rsv, t_allocator)
 
 #endif  // RIT_DYN_ARR_H_INCLUDED
+
+// Enable MSVC warning 4702: unreachable code
+#pragma warning(disable : 4702)
 
 /*
 The MIT License (MIT)
 
 Copyright 2024 Ritchiel Reza
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the “Software”), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the “Software”), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
